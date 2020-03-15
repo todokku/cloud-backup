a16b210ea3178a3cad950a25eebf80d694981c5b
xenud <lazzaronipaolo@outlook.it>
2020-03-15 10:27:05 +0100

    adding text prints

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@
 wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
 unzip -q rclone-v1.51.0-linux-amd64.zip
 export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
+
+echo "Installing rclone..."
+echo $PATH
