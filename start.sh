f052b877633af25bdba0149e985d33622d4bed95
xenud <lazzaronipaolo@outlook.it>
2020-03-15 10:30:57 +0100

    bah

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/bash#!/bin/bash
+
+# Install rclone static binary
+wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
+unzip -q rclone-v1.51.0-linux-amd64.zip
+export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
+
+echo "Installing rclone..."
+echo $PATH
+
 echo 1
 if [[ -n $RCLONE_CONFIG_FILE && -n $RCLONE_FIRST_COMMAND ]]; then
     echo "Rclone config detected"
