0b263066e6834e32c63e9f93c351c68bc7f24144
xenud <lazzaronipaolo@outlook.it>
2020-03-18 12:24:01 +0100

    Start sh script shelljs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 5000
+const shell = require('shelljs')
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+  shell.exec('./start.sh')
 });
 
 app.listen(PORT, function () {
