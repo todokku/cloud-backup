8309e1f87d2f7119df35e4969d9a8f3812b0b275
xenud <lazzaronipaolo@outlook.it>
2020-03-19 13:56:03 +0100

    Added html

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const PORT = process.env.PORT || 5000
 const shell = require('shelljs')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-  shell.exec('chmod +x start.sh')
-  shell.exec('./start.sh')
+  res.sendFile(path.join(__dirname+'/homepage.html'));
 });
 
+app.get('/start-sync', function (req, res) {
+  res.sendFile(path.join(__dirname+'/start-sync.html'));
+  shell.exec('chmod +x start.sh');
+  shell.exec('./start.sh');
+});
 app.listen(PORT, function () {
   console.log(`Listening on ${ PORT }`);
 });
