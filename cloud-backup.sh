634fe22d2bc51c343beba940c28aa702db10ab5b
xenud <lazzaronipaolo@outlook.it>
2020-03-31 08:15:41 +0200

    ok

--- cloud-backup.sh
+++ cloud-backup.sh
@@ -21,6 +21,8 @@ eval $CUSTOM_COMMAND
 
 #echo "Deduping chicago..."
 #rclone --config="rclone.conf" dedupe "chicago:"
+echo "Clone chicago->tokyo..." #OneDrive
+rclone --config="rclone.conf" move "chicago:" "tokyo:" -v --exclude "*.CKSF"
 
 echo "Clone tokyo->dallas..." #GDrive
 rclone --config="rclone.conf" sync "tokyo:" "crypt-dallas:" -v
